3cdcdbbd169978ba6f47c3afabae065842e13c54
Code4Bread <iz@kaungmyat.tech>
2019-04-16 10:40:39 +0800

    temporary landing page added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,32 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <React.Fragment>
+    <Row
+      type="flex"
+      align="middle"
+      justify="center"
+      style={{
+        marginTop: '25%',
+      }}
+    >
+      <Col
+        span={12}
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        <img alt="loader" src="https://loading.io/spinners/dna/index.dna-spin-spiral-preloader.svg" />
+        <h4
+          style={{
+            fontFamily: 'Roboto, sans-serif',
+          }}
+        >
+          Construction in progress
+        </h4>
+      </Col>
+    </Row>
+  </React.Fragment>
+);
