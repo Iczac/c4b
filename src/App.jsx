90caf2b0d003b535346e44fd20d5ffcbe6fc5c5b
Code4Bread <iz@kaungmyat.tech>
2019-04-16 16:49:55 +0800

    removed gatsby and new react build

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
