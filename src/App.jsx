22bed71d92bf720ddd06b72b4ef7723f1df9c42e
Code4Bread <iz@kaungmyat.tech>
2019-04-17 09:39:24 +0800

    updated for alignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+// noinspection ES6CheckImport - IDE Error
+import PropTypes from 'prop-types';
 import './App.css';
+import { Row, Col } from 'antd';
 
 class App extends React.PureComponent {
   render() {
+    // This prop is added just to solve eslint bug for now
+    const {
+      name,
+    } = this.props;
+
     return (
-      <div className="App">
-        Hello World
-      </div>
+      <Row
+        type="flex"
+        align="middle"
+        justify="center"
+        style={{
+          marginTop: '21%',
+        }}
+      >
+        <Col
+          span={12}
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          <img alt="loader" src="https://loading.io/spinners/dna/index.dna-spin-spiral-preloader.svg" />
+          <h4>
+            {name}
+          </h4>
+        </Col>
+      </Row>
     );
   }
 }
+
+App.propTypes = {
+  name: PropTypes.string,
+};
+
+App.defaultProps = {
+  name: 'Construction in progress',
+};
 
 export default App;
