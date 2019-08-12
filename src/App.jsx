7bb08d333267a9b09cff99aa8c722a3c4b02d515
Code4Bread <iz@kaungmyat.tech>
2019-08-12 16:15:47 +0800

    About me added to main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Profile from './components/Profile';
+import AboutMe from './components/AboutMe';
 import Footer from './components/Footer';
 
 const App = () => (
   <React.Fragment>
     <Profile />
+    <AboutMe />
     <Footer />
   </React.Fragment>
 );
