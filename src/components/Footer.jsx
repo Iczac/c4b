1deed96b8be663e1589eab1d53507fdc09a9b43d
Code4Bread <iz@kaungmyat.tech>
2019-08-12 16:15:26 +0800

    favourite quote added to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Row, Col, Icon } from 'antd';
+import {
+  Row,
+  Col,
+  Icon,
+  Typography,
+} from 'antd';
+
+const { Text } = Typography;
 
 const iconSize = 50;
 
@@ -19,6 +26,7 @@ const Footer = () => (
     justify="center"
     style={{
       marginTop: '5%',
+      textAlign: 'center',
     }}
   >
     <Col
@@ -27,9 +35,6 @@ const Footer = () => (
       <Row
         type="flex"
         justify="center"
-        style={{
-          textAlign: 'center',
-        }}
       >
         <Col
           span={6}
@@ -60,6 +65,14 @@ const Footer = () => (
           </a>
         </Col>
       </Row>
+      <br/>
+      <Text>
+        &#34;He who has a why to live can bear almost anyhow&#34;
+      </Text>
+      <br />
+      <Text>
+        - Friedrich Nietzsche
+      </Text>
     </Col>
   </Row>
 );
