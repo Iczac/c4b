8e3f002353cc49f2545115051343711a3b836ccd
Code4Bread <iz@kaungmyat.tech>
2019-08-12 13:14:07 +0800

    profile design changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
 import React from 'react';
-// noinspection ES6CheckImport - IDE Error
-import PropTypes from 'prop-types';
-import './App.css';
-import { Row, Col } from 'antd';
+import Profile from './components/Profile';
 
-class App extends React.PureComponent {
-  render() {
-    // This prop is added just to solve eslint bug for now
-    const {
-      name,
-    } = this.props;
-
-    return (
-      <Row
-        type="flex"
-        align="middle"
-        justify="center"
-        style={{
-          marginTop: '21%',
-        }}
-      >
-        <Col
-          span={12}
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          <img alt="loader" src="https://loading.io/spinners/dna/index.dna-spin-spiral-preloader.svg" />
-          <h4>
-            {name}
-          </h4>
-        </Col>
-      </Row>
-    );
-  }
-}
-
-App.propTypes = {
-  name: PropTypes.string,
-};
-
-App.defaultProps = {
-  name: 'Under Development',
-};
+const App = () => (
+  <Profile />
+);
 
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Col,
+  Row,
+  Typography,
+} from 'antd';
+
+const { Title } = Typography;
+
+const Profile = () => (
+  <Row
+    type="flex"
+    justify="center"
+    style={{
+      marginTop: '2%',
+    }}
+  >
+    <Col
+      span={12}
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <Title
+        style={{
+          marginBottom: 0,
+        }}
+      >
+        Code4Bread
+      </Title>
+      <Title
+        level={4}
+        style={{
+          marginTop: 0,
+        }}
+      >
+        Pragmatic Developer
+      </Title>
+    </Col>
+  </Row>
+);
+
+export default Profile;
