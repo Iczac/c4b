106b444c059d2dd36dcfcdfeba3bd8ce88abcb63
Code4Bread <iz@kaungmyat.tech>
2020-05-26 04:27:08 +0800

    profile added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import Profile from './components/Profile';
-import AboutMe from './components/AboutMe';
-import Footer from './components/Footer';
+import React, { useState } from 'react';
+import { Box } from '@material-ui/core';
+import Styled from 'styled-components';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
 
-const App = () => (
-  <React.Fragment>
-    <Profile />
-    <AboutMe />
-    <Footer />
-  </React.Fragment>
-);
+const StyledBox = Styled(Box)`
+  margin-left: 20%;
+  margin-right: 20%;
+`;
+
+const App = () => {
+  const [page, setPage] = useState('aboutme');
+
+  let pageView;
+
+  if (page === 'exp') {
+    pageView = <h1>Hello Exp</h1>;
+  } else if (page === 'projects') {
+    pageView = <h1>Hello Projects</h1>;
+  } else {
+    pageView = <MainPage />;
+  }
+
+  return (
+    <StyledBox>
+      <Header setPage={setPage} page={page} />
+      {pageView}
+    </StyledBox>
+  );
+};
 
 export default App;
